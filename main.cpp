c7ae90ea4907a3d539e5cb80aa983824295f5331
GGene-E <eugenetyeweechin@gmail.com>
2022-02-17 00:45:55 +0800

    test
    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! ehehehehehehe GOD DAMN IT" << std::endl;
+    std::cout << "Hello, World! ehehehehehehe GOD DAMN IT GITHUB WHY" << std::endl;
     return 0;
 }
